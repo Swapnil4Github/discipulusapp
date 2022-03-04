c1df844e4766fe047ead888f39cd7cf6f2231c30
Swapnil4Github <swapnil4yc@gmail.com>
2022-03-04 13:02:10 +0530

    Start

---
+++ lib/authentication/viaGmail.dart
@@ -0,0 +1,164 @@
+import 'dart:js';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:discipulus/SignUpScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  loginWithGoogle() async {
+      final GoogleSignInAccount? googleSignInAccount =
+      await googleSignIn.signIn();
+      final googleSignInAuthentication = await googleSignInAccount!.authentication;
+      final AuthCredential creds = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken
+      );
+      // final AuthCredential credential = GoogleAuthProvider.getCredential(
+      //   accessToken: googleSignInAuthentication.accessToken,
+      //   idToken: googleSignInAuthentication.idToken,
+      // );
+      final authResult = await _auth.signInWithCredential(creds);
+      await FirebaseAuth.instance.signInWithCredential(creds);
+
+      // await FirebaseAuth.instance.signInWithCredential(creds).then((value) => Get.offAll(Dashboard(),arguments: _auth.currentUser));
+    // final User? user = authResult.user;
+      // assert(await user?.getIdToken() != null);
+      // final User? currentUser = _auth.currentUser;
+      // assert(user?.uid == currentUser?.uid);
+      // Get.to
+      // return Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       CircleAvatar(
+      //         backgroundImage: NetworkImage(googleSignInAccount.photoUrl.toString()),
+      //       ),
+      //       Text(googleSignInAccount.displayName.toString()),
+      //       SizedBox(height: 30,),
+      //       GestureDetector(
+      //         onTap: ()async{
+      //           await googleSignIn.signOut();
+      //           await FirebaseAuth.instance.signOut();
+      //
+      //         },
+      //         child: const Padding(padding: EdgeInsets.all(8.0),
+      //           child: Text('Logout'),),
+      //
+      //       )
+      //     ],
+      //   ),
+      //
+      // );
+    }
+
+
+  // Future<void> logoutGoogle() async {
+  //   await googleSignIn.signOut();
+  //   Get.back(); // navigate to your wanted page after logout.
+  // }
+
+}
+// class Dashboard extends StatefulWidget {
+//   const Dashboard({Key? key}) : super(key: key);
+//
+//   @override
+//   _DashboardState createState() => _DashboardState();
+// }
+
+// class _DashboardState extends State<Dashboard> {
+//   late User user;
+//   @override
+//   void onInit() async {
+//     user = Get.arguments;
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Center(
+//       child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//       CircleAvatar(
+//       backgroundImage: NetworkImage(user.photoURL.toString()),
+//       ),
+//       Text(user.displayName.toString()),
+//       SizedBox(height: 30,),
+//       GestureDetector(
+//       onTap: ()async{
+//       await googleSignIn.signOut();
+//       await FirebaseAuth.instance.signOut();
+//
+//       },
+//       child: const Padding(padding: EdgeInsets.all(8.0),
+//       child: Text('Logout'),),
+//
+//       )
+//       ],
+//       ),
+//
+//       ),
+//     );
+//   }
+// }
+
+// class AuthController extends GetxController {
+//   static AuthController instance = Get.find();
+//   GoogleSignIn _googleSignIn = GoogleSignIn();
+//   FirebaseAuth _check = FirebaseAuth.instance;
+//   late Rx<User?> _user;
+//
+//   @override
+//   void onReady()
+//   {
+//     super.onReady();
+//     _user = auth.currentUser as Rx<User?>;
+//     _user.bindStream(auth.userChanges());
+//     ever(_user,_initialScreen);
+//   }
+//   void signInGmail()async{
+//     final newuser = await _googleSignIn.signIn();
+//     final googleauth = await newuser!.authentication;
+//     final creds = GoogleAuthProvider.credential(
+//         accessToken: googleauth.accessToken,
+//         idToken: googleauth.idToken
+//     );
+//     await FirebaseAuth.instance.signInWithCredential(creds);
+//
+//   }
+//
+//
+//
+//
+// }
+
+
+
+
+// Center(
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// CircleAvatar(
+// backgroundImage: NetworkImage(snapshot.data!.photoURL.toString()),
+// ),
+// Text(snapshot.data!.displayName.toString()),
+// SizedBox(height: 30,),
+// GestureDetector(
+// onTap: ()async{
+// await _googleSignIn.signOut();
+// await FirebaseAuth.instance.signOut();
+//
+// },
+// child: const Padding(padding: EdgeInsets.all(8.0),
+// child: Text('Logout'),),
+//
+// )
+// ],
+// ),
+//
+// );
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:discipulus/authentication/viaEmail.dart';
+import 'package:flutter/material.dart';
+import 'SignUpScreen.dart';
+import 'package:flutter/painting.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpScreen(),
+    );
+  }
+}
+
+
+class Palette {
+  static const Color iconColor = Color(0xFFB6C7D1);
+  static const Color activeColor = Color(0xFF09126C);
+  static const Color textColor1 = Color(0XFFA7BCC7);
+  static const Color textColor2 = Color(0XFF9BB3C0);
+  static const Color facebookColor = Color(0xFF3B5999);
+  static const Color googleColor = Color(0xFFDE4B39);
+  static const Color backgroundColor = Color(0xFFECF3F9);
+ 
+}
